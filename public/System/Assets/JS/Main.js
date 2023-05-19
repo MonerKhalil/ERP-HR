92ad0bc1ed2112c421b62bfce374c38a578ef6ff
AmirHO3345 <106338740+AmirHO3345@users.noreply.github.com>
2023-05-19 16:51:29 +0300

    Bundle_9-1
    Fix Some Conflict From Master

--- public/System/Assets/JS/Main.js
+++ public/System/Assets/JS/Main.js
@@ -1561,7 +1561,6 @@ $(document).ready(function (){
         });
     });
 
-
 });
 
 window.onload = function (){
@@ -1575,40 +1574,6 @@ window.onload = function (){
 
     GetFullParams() ;
 }
-    /*===========================================
-    =           Fields Visibility        =
-    =============================================*/
-
-    $(".VisibilityOption").ready(function () {
-        $(".VisibilityOption").each((_ , VisibilityOption) => {
-            const TargetName = $(VisibilityOption).attr("data-ElementsTargetName");
-            $(VisibilityOption).find(".Selector").each((_ , Selectors) => {
-                $(Selectors).find(".Selector__Option").each((_ , Option) => {
-                    const ValueOption = $(Option).attr("data-option") ;
-                    $(Option).click(() => {
-                        TriggerName(TargetName , ValueOption);
-                    });
-                });
-                TriggerName(TargetName , '') ;
-            });
-            $(VisibilityOption).find(".CheckBox__Input").on('change', ()=>{
-                TriggerName(TargetName , $(this).val());
-            })
-        });
-    });
-
-    function TriggerName(NameElement = String , ValueSelected = String) {
-        $(".VisibilityTarget").each((_ , VisibilityTarget) => {
-            const ElementName =  $(VisibilityTarget).attr("data-TargetName") ;
-            if(ElementName === NameElement) {
-                const ElementValue = $(VisibilityTarget).attr("data-TargetValue") ;
-                $(VisibilityTarget).hide();
-                console.log(ValueSelected , ElementValue) ;
-                if(ValueSelected === ElementValue)
-                    $(VisibilityTarget).show();
-            }
-        });
-    }
 
 
 /*===========================================
